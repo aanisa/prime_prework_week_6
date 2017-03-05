c33255a9521b7a36312808f78c7815ec6334e07a
Anisa <Anisa@Anisas-MacBook-Pro.local>
2017-03-04 20:01:11 -0600

    Added code

--- assignment/1_selectors/script.js
+++ assignment/1_selectors/script.js
@@ -11,22 +11,28 @@ var onReady = function() {
 
   // 1 - hide just the h1 inside header
   // write your code for question 1 here.
+$('header').hide();
 
   // 2 - change the background color of 'step-two' to yellow
   // write your code for question 2 here.
+$('#step-two').css('text-color', 'yellow');
 
   // 3 - fadout example elements
   // 3 bonus - wait one second and fade back in
   // write your code for question 3 here.
+$('example-list').fadeOut('fast').delay(1000).fadeIn('fast');
 
   // 4 - select all odd list items and change to blue
   // write your code for question 4 here.
+$('li:nth-child(odd)').css('text-color', 'blue');
 
   // 5 - select the last list item with a pseudo selector
   // write your code for question 5 here.
+$('li:last').select();
 
   // 6 - select the last list item with traversal
   // write your code for question 6 here.
+$('ui').children('li.last');
 };
 
 // When the DOM is loaded, our code in the onReady function will run.
--- assignment/2_burrito-cat/script.js
+++ assignment/2_burrito-cat/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+                                  //NEED TO ADD .CLICK SO WILL WORK WHEN CLICK BUTTON
   // nav bar event listeners set up
   $('.navDiv').mouseenter(mouseEnterButton);
   $('.navDiv').mouseleave(mouseLeaveButton);
@@ -11,6 +11,12 @@ $(document).ready(function() {
   $( '#hideDiv' ).on( 'click', function(){
     hideCat();
   });
+  $( '#animateDiv' ).on( 'click', function(){
+    animateCat();
+  });
+  $( '#resetDiv' ).on( 'click', function(){
+    resetCat();
+  });
 });
 
 // nav bar function to fade when mouse enters button
@@ -21,8 +27,8 @@ function mouseEnterButton() {
 
 // nav bar function to fade when mouse enters button
 function mouseLeaveButton() {
-  console.log('leave');
   $(this).fadeTo('fast', 1);
+  console.log('leave');
 }
 
 // hideCat is a function to hide the cat image when that button is clicked
@@ -30,6 +36,8 @@ function hideCat() {
   //TODO your function code here
   // hide catImg
   // append '<p>hide toggle</p>' to 'clickList'
+  $('#catImg').hide();
+  $('clickList').append("<p>hide toggle</p>");
 }
 
 // fadeCat is a function to fade cat in or out when that button is clicked
@@ -37,7 +45,8 @@ function fadeCat() {
   //TODO your function code here
   // toggle catImg fade
   // append '<p>fade toggle</p>' to 'clickList'
-
+  $('#catImg').toggleFade();
+  $('clickList').append("<p>fade toggle</p>");
 }
 
 // animateCat is a function to grow the cat's height and width by 10px when that button is clicked
@@ -45,12 +54,18 @@ function animateCat() {
   //TODO your function code here
   // animate catImg
   // append '<p>animate</p>' to 'clickList'
+  $('#catImg').animate(function() {
+    $(this).height('10px');
+    $(this).width('10px');
+  $('clickList').append("<p>animate</p>");
+});
 }
-
 // PRO MODE
 // resetCat is a function to reset the cat photo to it's original size
 // when that button is clicked.
 function resetCat() {
   // reset catImg
   // append '<p>reset</p>' to 'clickList'
+  $('#catImg').reset();
+  $('clickList').append("<p>reset</p>");
 }
